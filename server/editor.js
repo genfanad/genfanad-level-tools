f116981dd30718564dbcb5f7bbf6c3c47b58f74b
Andrei <akrotkov@gmail.com>
2020-09-15 20:16:18 -0700

    Adding client/server code separation

--- server/editor.js
+++ server/editor.js
@@ -11,6 +11,8 @@ app.set('port', PORT);
 //app.use(bodyParser.json({limit: '50mb', extended: true}))
 
 app.use(express.static(path.join(__dirname,'../web')));
+app.use('/client', express.static(path.join(__dirname,'../client')));
+app.use('/workspaces', express.static(path.join(__dirname,'../tmp')));
 
 var server = http.Server(app);
 server.listen(PORT, function() {
