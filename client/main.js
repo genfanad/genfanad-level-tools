3641b98c7db38b765952ab40bc33afc4df1a5720
Andrei <akrotkov@gmail.com>
2020-09-16 18:23:25 -0700

    Adding basic plumbing for new/open/reload/save

--- client/main.js
+++ client/main.js
@@ -1,3 +1,23 @@
 $(document).ready( () => {
     SCENE.init();
+    WORKSPACES.init();
 })
+
+function get(uri, oncomplete) {
+    $.ajax({
+        url: uri,
+        type: 'GET',
+        contentType: 'application/json', 
+        success: (f) => oncomplete(JSON.parse(f))
+    });
+}
+
+function post(uri, data, oncomplete) {
+    $.ajax({
+        url: uri,
+        type: 'POST',
+        data: JSON.stringify(data),
+        contentType: 'application/json', 
+        success: (f) => oncomplete(JSON.parse(f))
+    });
+}
---
+++ client/workspaces.js
@@ -0,0 +1,55 @@
+class Workspaces {
+    constructor() {
+        this.opened = undefined;
+    }
+
+    closeWorkspace() {
+        console.log("Closing " + this.opened);
+
+        if (!this.opened) return;
+
+        this.opened = undefined;
+    }
+
+    openWorkspace(name) {
+        if (this.opened == name) return;
+
+        this.closeWorkspace();
+
+        console.log("Opening " + name);
+        this.opened = name;
+    }
+
+    create() {
+        console.log('TODO: new');
+    }
+
+    open() {
+        console.log('TODO: open');
+    }
+
+    reload() {
+        console.log('TODO: reload');
+    }
+
+    save() {
+        console.log('TODO: save');
+    }
+
+    init() {
+        get('/api/workspaces/list', (data) => {
+            let list = document.getElementById('file-workspaces');
+            list.innerHTML = "";
+            for (let workspace of data) {
+                let e = document.createElement('li');
+                e.innerText = workspace;
+                list.appendChild(e);
+            }
+            $('#file-workspaces').datalist({
+                onSelect: (n,r) => this.openWorkspace(r.value)
+            });
+        });
+    }
+}
+
+var WORKSPACES = new Workspaces();
