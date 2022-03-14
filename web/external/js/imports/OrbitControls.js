d2bd2fe0b40fdb16a1a96645c31604a276bea416
Devendra Venus <devendravoss@outlook.com>
2022-03-14 17:28:50 -0600

    UI and Orbit Control updated

--- web/external/js/imports/OrbitControls.js
+++ web/external/js/imports/OrbitControls.js
@@ -75,7 +75,7 @@ THREE.OrbitControls = function ( object, domElement ) {
 	this.keys = { LEFT: 37, UP: 38, RIGHT: 39, BOTTOM: 40 };
 
 	// Mouse buttons
-	this.mouseButtons = { ORBIT: THREE.MOUSE.LEFT, ZOOM: THREE.MOUSE.MIDDLE, PAN: THREE.MOUSE.RIGHT };
+	this.mouseButtons = { ORBIT: THREE.MOUSE.LEFT, PAN: THREE.MOUSE.MIDDLE, ZOOM: THREE.MOUSE.RIGHT };
 
 	// for reset
 	this.target0 = this.target.clone();
@@ -680,7 +680,7 @@ THREE.OrbitControls = function ( object, domElement ) {
 
 		if ( scope.enabled === false ) return;
 
-		if (!event.shiftKey) return;
+		if( !event.shiftKey ) return;
 
 		event.preventDefault();
 
